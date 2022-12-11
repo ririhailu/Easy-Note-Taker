c770e1b0780bba792d6ed305cf2c8270bcc7ddb1
Matrix <rhailu88@gmail.com>
2022-12-11 15:26:09 -0500

    Added function to create new note in server.js file.

--- server.js
+++ server.js
@@ -18,3 +18,13 @@ app.get('/api/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
