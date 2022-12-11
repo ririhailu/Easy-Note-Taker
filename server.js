15272c0b6c04cdc7a182b19882a3f9e6cae0f84a
Matrix <rhailu88@gmail.com>
2022-12-11 14:53:52 -0500

    Created way to option to choose parsing URL with query if true in server.js file.

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const express = require('express');
 const app = express();
 
 const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
